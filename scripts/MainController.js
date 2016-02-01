87c03c8b9509b496c1f777621be1f7a2fe421f83
indu chaurasia <indu.bitmesra@gmail.com>
2016-02-02 00:02:25 +0530

    added previous

--- scripts/MainController.js
+++ scripts/MainController.js
@@ -2,100 +2,14 @@
 
 	'use strict';
 
-	angularFormlyApp.controller('MainController', function($scope, province) {
+	angularFormlyApp.controller('MainController', function($scope, formService) {
 
 		$scope.personDetails = {};
-		$scope.personAddress = {};
+		$scope.personOccupation={};
 
-	$scope.personFields = {
-			step1: [
-			{
-			key : 'firstName',
-			type : 'input',
-			templateOptions : {
-				type : 'text',
-				label : 'First Name',
-				placeholder : 'Enter your first name',
-				required : true
-			}
-		}, {
-			key : 'lastName',
-			type : 'input',
-			templateOptions : {
-				type : 'text',
-				label : 'Last Name',
-				placeholder : 'Enter your last name',
-				required : true
-			}
-		},
+		$scope.personDetailFormFields =  formService.getPersonDetailForm();
+		$scope.personOccupationFormFields = formService.getPersonOccupationForm();
 
-		 {
-			key : 'email',
-			type : 'input',
-			templateOptions : {
-				type : 'email',
-				label : 'Email address',
-				placeholder : 'Enter email',
-				required : true
-			}
-		},
-
-		{
-		 key : 'dateOfBirth',
-		 type : 'input',
-		 templateOptions : {
-			 type : 'date',
-			 label : 'Date of birth',
-			 placeholder : 'date of birth ',
-			 required : true
-		 	}
-		},
-
-		{
-			key: 'height',
-			type: 'input',
-			templateOptions:{
-				type: 'number',
-				label: 'Height',
-				placeholder: 'Enter height',
-				required: true
-			}
-		},
-
-		{
-			key: 'weight',
-			type: 'input',
-			templateOptions:{
-				type: 'number',
-				label: 'Weight',
-				placeholder: 'Enter weight',
-				required: true
-			}
-	}
-
-
-	],
- step2:[
-	 	{
-			key: 'address'
-			type: 'textarea',
-			templateOptions : {
-				type: 'text',
-				required: true,
-				label: 'Full address',
-				placeholder : 'Enter your full address',
-			}
-		},
-		{
-			key : 'province',
-			type : 'typeahead',
-			templateOptions : {
-				label : 'Province',
-				options : province.getProvinces()
-			}
-		}
-	]
-};
 
 	})
 
