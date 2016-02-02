ee070b3a9ba1877b4f26d41cb3fe0241cf65c420
indu chaurasia <indu.bitmesra@gmail.com>
2016-02-03 00:17:47 +0530

    added dynamic questions, which renders dynamic json

--- scripts/MainController.js
+++ scripts/MainController.js
@@ -6,10 +6,11 @@
 
 		$scope.personDetails = {};
 		$scope.personOccupation={};
+		$scope.medicalConditions ={};
 
 		$scope.personDetailFormFields =  formService.getPersonDetailForm();
 		$scope.personOccupationFormFields = formService.getPersonOccupationForm();
-
+		$scope.medicalConditionsFields = formService.getMedicalQuestionsForm();
 
 	})
 
